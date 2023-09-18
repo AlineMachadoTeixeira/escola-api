2058df13ddeada63038a8829a602666f9913fa8c
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-09-18 12:03:50 -0300

    Alunos NodeJs

---
+++ README.md
@@ -0,0 +1,24 @@
+# Escola Api 
+
+API utilizando NodeJs + Express
+
+## Fizemos 
+
+#### CMD Para criar o PACKAGE.JSON
+- npm init
+- expressjs
+- version (1.0.0) entre
+- descrição (pode ser - API NODEJS + EXPRESS com CRUD)
+- entra point (pasta que está) escreve app.js  (O nome dessa pasta é o lugar que você tem que colocar import express from 'express';) 
+- vai dando entrer (testecommand:  / git repository: / keywords: / autor: / license(ISC)) 
+- entre
+
+##### Na pasta PACKAGE.JSON que foi criado  coloque "type":"module
+
+#### Baixar o EXPRESS
+- no CMD na pasta que você quer digite  para instalar os molulos do express
+- npm i express e entre
+
+#### Prenchendo o arquivo index.js (OBS: No PACKAGE.JSON na parte do  "main": "index.js" está com nome que precisamos usar na pasta ,)
+
+#### No CMD colocamos npx nodemon  entre  (para ver se está rodando no site http://localhost:8080/ assim que fizemos a Executando o servidor )
---
+++ index.js
@@ -0,0 +1,47 @@
+import express from 'express';
+
+//armazenando o express em uma variável. Ao inves de chamar express().get, usaremos app.get;
+const app = express();
+
+//Criando AS ROTAS 
+
+//Raiz da aplicação
+app.get('/', (req, res) => {
+    res.send (`Raiz da API NodeJS + Express + MySQL`);
+});
+
+//Todos os alunos 
+app.get('/alunos', (req, res) => {
+    res.send (`Exibindo dados de todos os alunos`)
+});
+
+//Um aluno 
+app.get('/alunos/:id', (req, res) => {
+    res.send (`Exibindo dados de UM aluno`)
+});
+
+//Inserindo NOVO aluno com POST 
+app.post('/alunos', (req, res) => {
+    res.send (`Inserindo UM aluno`)
+});
+
+//Atualizando Dados de um aluno  com patch
+app.patch('/alunos/:id', (req, res) => {
+    res.send (`Atualizando dados de Um aluno `)
+});
+
+
+//Excluindo Aluno com delete
+app.delete('/alunos/:id', (req, res) => {
+    res.send (`Excluindo alunos `)
+});
+
+//Executando o servidor 
+const porta = 8080;
+// porta é a const 8080 está lá em cima  http://localhost:8080/
+app.listen(porta, () => {
+    console.log(`Servidor NodeJS rodando na porta ${porta}`);
+});
+
+
+
